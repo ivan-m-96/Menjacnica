80872c0a296186a3af40efa0e1e02080262fef0f
ivan-m-96 <ivan.m.96@outlook.com>
2017-03-30 21:47:19 +0200

    Generisane toString(takodje i izmenjena), hashCode i equals metode

--- Menjacnica/src/resursi/Valuta.java
+++ Menjacnica/src/resursi/Valuta.java
@@ -45,5 +45,58 @@ public class Valuta {
 	public void setSredKurs(double sredKurs) {
 		this.sredKurs = sredKurs;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((datumVal == null) ? 0 : datumVal.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(kupKurs);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((naziv == null) ? 0 : naziv.hashCode());
+		temp = Double.doubleToLongBits(prodKurs);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((skrVal == null) ? 0 : skrVal.hashCode());
+		temp = Double.doubleToLongBits(sredKurs);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Valuta other = (Valuta) obj;
+		if (datumVal == null) {
+			if (other.datumVal != null)
+				return false;
+		} else if (!datumVal.equals(other.datumVal))
+			return false;
+		if (Double.doubleToLongBits(kupKurs) != Double.doubleToLongBits(other.kupKurs))
+			return false;
+		if (naziv == null) {
+			if (other.naziv != null)
+				return false;
+		} else if (!naziv.equals(other.naziv))
+			return false;
+		if (Double.doubleToLongBits(prodKurs) != Double.doubleToLongBits(other.prodKurs))
+			return false;
+		if (skrVal == null) {
+			if (other.skrVal != null)
+				return false;
+		} else if (!skrVal.equals(other.skrVal))
+			return false;
+		if (Double.doubleToLongBits(sredKurs) != Double.doubleToLongBits(other.sredKurs))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Valuta: " + naziv + " " + skrVal + " " + datumVal.getTime() + " prodajni kurs:" + prodKurs
+				+" srednji kurs: " + sredKurs + " kupovni kurs: " + kupKurs;
+	}
 	
 }
