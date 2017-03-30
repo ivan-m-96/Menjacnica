981cd9319062203d4dd63bf39e565bf190158c88
ivan-m-96 <ivan.m.96@outlook.com>
2017-03-30 22:07:03 +0200

    Ubacena klasa RadSaValutama koja implementira interfejs
    InterfejsMenjacnica

---
+++ Menjacnica/src/resursi/RadSaValutama.java
@@ -0,0 +1,29 @@
+package resursi;
+
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+
+import interfejs.InterfejsMenjacnica;
+
+public class RadSaValutama implements InterfejsMenjacnica {
+
+	@Override
+	public void dodajKurs(ArrayList<Valuta> valute, String nazVal, String skrVal, double kupKurs, double sredKurs,
+			double prodKurs, GregorianCalendar datumVal) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void izbrisiKurs(ArrayList<Valuta> valute, String skrVal, GregorianCalendar datumVal) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Valuta pronadjiKurs(ArrayList<Valuta> valute, String skrVal, GregorianCalendar datVal) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
